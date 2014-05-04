a717d65aac6be13f84e7e82805184e3bb7cf5b39
Nattawat Nonsung <armmer1@gmail.com>
2014-05-04 21:05:09 +0700

    - Make it work with short content - Not introduce any options

--- src/fixedbg.js
+++ src/fixedbg.js
@@ -8,31 +8,57 @@
 
 (function ($) {
 
+  var setHeightToWindowSize = function(dom){
+    var paddingTop = dom.css('padding-top');
+    paddingTop = +paddingTop.substring(0, paddingTop.length -2);
+
+    var paddingButtom = dom.css('padding-bottom');
+    paddingButtom = +paddingButtom.substring(0, paddingButtom.length -2);
+
+    var height = $(window).innerHeight() - paddingTop - paddingButtom
+    dom.height(height);
+
+    // el's top position
+    var elTop = dom.offset().top; 
+    dom.data("elTop", elTop);
+
+    // el's bottom position
+    var elBottom = elTop + height + paddingTop + paddingButtom; 
+    dom.data("elBottom", elBottom);
+  }
+
   // Collection method.
-  $.fn.awesome = function () {
-    return this.each(function (i) {
-      // Do something awesome to each selected element.
-      $(this).html('awesome' + i);
+  $.fn.fixedBG = function (options) {
+    var self = this;
+    // Define default setting
+    var settings = $.extend({
+      
+    }, options );
+
+    this.each(function(){
+      setHeightToWindowSize($(this));
     });
-  };
 
-  // Static method.
-  $.awesome = function (options) {
-    // Override default options with passed-in options.
-    options = $.extend({}, $.awesome.options, options);
-    // Return something awesome.
-    return 'awesome' + options.punctuation;
-  };
+    $(window).resize(function(){
+      self.each(function(){
+        setHeightToWindowSize($(this));
+      });
+    });
 
-  // Static method default options.
-  $.awesome.options = {
-    punctuation: '.'
-  };
+    $(window).scroll(function(){
+      var docViewTop = $(window).scrollTop();
 
-  // Custom selector.
-  $.expr[':'].awesome = function (elem) {
-    // Is this element awesome?
-    return $(elem).text().indexOf('awesome') !== -1;
+      self.each(function(p){
+        var elTop = $(this).data("elTop");
+        var elBottom = $(this).data("elBottom");
+
+        if ( docViewTop >= elTop && elBottom > docViewTop) {
+          self.css("background-attachment", "scroll");
+          // div is in the visible window
+          $(this).css("background-attachment", "fixed");
+        }
+      });
+    });
   };
 
 }(jQuery));
