8f23e1291ddb1d7fb8db92111dd09ce99936301a
Nattawat Nonsung <armmer1@gmail.com>
2014-05-13 15:17:53 +0700

    Make test passed

--- test/fixedbg_test.js
+++ test/fixedbg_test.js
@@ -20,43 +20,22 @@
       throws(block, [expected], [message])
   */
 
-  module('jQuery#awesome', {
+  module('jQuery#fixedBG', {
     // This will run before each test in this module.
     setup: function() {
-      this.elems = $('#qunit-fixture').children();
+      this.elems = $('.page');
     }
   });
 
   test('is chainable', function() {
     expect(1);
     // Not a bad test to run on collection methods.
-    strictEqual(this.elems.awesome(), this.elems, 'should be chainable');
+    strictEqual(this.elems.fixedBG(), this.elems, 'should be chainable');
   });
 
-  test('is awesome', function() {
+  test('the first element', function() {
     expect(1);
-    strictEqual(this.elems.awesome().text(), 'awesome0awesome1awesome2', 'should be awesome');
-  });
-
-  module('jQuery.awesome');
-
-  test('is awesome', function() {
-    expect(2);
-    strictEqual($.awesome(), 'awesome.', 'should be awesome');
-    strictEqual($.awesome({punctuation: '!'}), 'awesome!', 'should be thoroughly awesome');
-  });
-
-  module(':awesome selector', {
-    // This will run before each test in this module.
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
-    }
-  });
-
-  test('is awesome', function() {
-    expect(1);
-    // Use deepEqual & .get() when comparing jQuery objects.
-    deepEqual(this.elems.filter(':awesome').get(), this.elems.last().get(), 'knows awesome when it sees it');
+    strictEqual(this.elems.fixedBG().first().css("background-attachment"), "fixed", 'should be fixed');
   });
 
 }(jQuery));
